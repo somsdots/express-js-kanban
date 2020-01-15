ab9b7b6ff99eec0038c4a2663d6223424c803615
Admin <admin@somiksharma>
2020-01-15 13:57:32 +0530

    Socket Module Added with PubNub Intgration for Realtime Stock Price Updated Chart

--- app/index.js
+++ app/index.js
@@ -15,11 +15,18 @@ const cors = require('cors');
 const api = attract('core/routes/api/v1');
 const session = require('express-session');
 attract('config/passport')(passport);
+const PubNub = require('pubnub');
 
 const [app, port] = [express(), process.env.PORT || 9000];
 
 //Connect Db
 mongoose.connect(config.database.url, { useCreateIndex: true, useNewUrlParser: true });
+
+//Create Socket
+const http = require('http');
+const server = http.createServer(app);
+//creates a new socket.io instance attached to the http server.
+const io = require('socket.io').listen(server);
 
 //Set App wide configs
 app.locals.pretty = process.env.PRETTY_HTML === 'true';
@@ -78,5 +85,37 @@ app.use(
 	})
 );
 
+//Whenever someone connects this gets executed
+io.on('connection', function (socket) {
+	var strData;
+	//Instantiate a new Pubnub instance along with the subscribeKey 
+	pubnub = new PubNub({
+        subscribeKey : 'sub-c-4377ab04-f100-11e3-bffd-02ee2ddab7fe'
+    })
+    //adding listener to pubnub
+    pubnub.addListener({
+        message: function(message) {
+			/*checking whether the message contains data for the ‘Apple’ category or not.*/
+        	if(message.message.symbol=='Apple'){
+                /*Creates a new date object from the specified message.timestamp.*/ 
+        		var x = new Date(message.message.timestamp);
+				//Converting the timestamp into a desired format. HH:MM:SS:MS
+				var formatted =  (x.getHours()) + ':' + (x.getMinutes()) + ':' + (x.getSeconds()) + ':' + (x.getMilliseconds());
+                /*Here we are creating an object which will contain a timestamp as label and the ‘order_quantity’ as value.*/
+        		strData = {"label": formatted,
+						   "value":message.message.order_quantity
+						}
+                //sending data to the client
+				socket.emit('news', strData);
+        	};  
+        }
+    })      
+    console.log("Subscribing..");
+	//Subscribe the PubNub channel
+    pubnub.subscribe({
+        channels: ['pubnub-market-orders'] 
+    });
+});
+
 //Start Express Server
-app.listen(port, log.info.bind(null, ` Up on port: ${port}`));
+server.listen(port, log.info.bind(null, ` Up on port: ${port}`));
